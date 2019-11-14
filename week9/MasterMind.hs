df40182309b220bc44dbd86ed1675fc38b300f2e
DavidBergevoet <epicman008@gmail.com>
2019-11-14 16:36:17 +0100

    HUGE FIX

--- week9/MasterMind.hs
+++ week9/MasterMind.hs
@@ -7,9 +7,24 @@ import System.Random
 import Data.Char
 import Data.List
 
+-- This is the base function which executes the mastermind functions and asks for the variables
+mastermind :: IO()
+mastermind = do
+  putStr "Enter code length: "
+  len <- getLine
+  putStr "Enter code numbers 1.."
+  nr <- getLine
+  putStr "Enter number of tries: "
+  tries <- getLine
+  z <- (breakCode (read tries :: Int) (genCode (read len :: Int ) (read nr :: Int)))
+  print z
+  return ()
+
+-- Generate a number between 1 and the given number
 dice :: Int ->IO Int
 dice x = getStdRandom (randomR (1,x))
 
+-- This will generate a code with a given length and the maximal number in the code
 genCode :: Int -> Int -> IO[Int]
 genCode 0 _ = return []
 genCode len cnt = do
@@ -17,6 +32,7 @@ genCode len cnt = do
   rs <- genCode (len-1) cnt
   return (r:rs)
 
+-- This lets the user break a given code and returns Failed or Correct based on the tries and code
 breakCode :: Int -> IO[Int] -> IO [Char]
 breakCode 0 code = return " Failed"
 breakCode tries code = do
@@ -37,14 +53,28 @@ breakCode tries code = do
     else
       breakCode (tries-1) (return z)
 
-
+-- This will convert a string to an int array
 strToIntArr :: String -> [Int]
 strToIntArr str =  map (\x -> read x :: Int) (words str)
 
+-- This will calculate the number of white pins with a given guess and code
 whitePins :: [Int] -> [Int] -> Int
-whitePins guess code = sum( map(\x -> if elem x code then 1 else 0) guess) - (redPins guess code)
+whitePins guess code =
+  let innerCompare x y = if (count x code) < (count x guess) then (count x code) else (count x guess) in
+    let compare x y = if x == y then (innerCompare x y) else 0 in
+      let innerMap x = map (\y -> compare x y) (unique code) in
+        sum (concat(map (\x-> innerMap x ) (unique guess))) - (redPins guess code)
 
+-- This will make a list with only the unique elements
+unique :: Eq a => [a] -> [a]
+unique []       = []
+unique (x : xs) = x : unique (filter (x /=) xs)
 
+-- This will count the number of elements in a given list
+count :: Ord a => a -> [a] -> Int
+count x xs = (length . filter (== x)) xs
+
+-- This will calculate the number of red pins based on a guess and code
 redPins :: [Int] -> [Int] -> Int
 redPins [] _ = 0
 redPins (x:xs) (y:ys)
